d6cb6c391d531979d804d84800a974bf8df643a1
Diego Avendaño <digago7@gmail.com>
2023-11-29 18:51:08 -0600

    children comments

---
+++ src/lib/components/CardComment.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let message
+  export let username
+  export let likes
+  export let commentId
+  export let userId
+  export let son
+  export let nieto
+</script>
+
+<div class="card w-96 bg-gray-700 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">{message}</h2>
+    <p>author: {username}</p>
+    <p>Likes: {likes}</p>
+    <div class="card-actions justify-end">
+      <form method="POST" action="/posts/[slug]?/comentarComentario">
+        <button class="btn btn-primary" formaction="/posts/[slug]?/like"
+          >Like</button
+        >
+        <button class="btn btn-primary" formaction="/posts/[slug]?/dislike"
+          >Dislike</button
+        >
+        <br />
+        <input type="hidden" name="parentId" value={commentId} />
+        <input type="hidden" name="userId" value={userId} />
+        <input type="text" name="message" placeholder="leave a comment" />
+        <button class="btn btn-primary" type="submit">Send</button>
+      </form>
+      {#if son}
+        <p>I am a son</p>
+      {/if}
+      {#if nieto}
+        <p>I am a nieto</p>
+      {/if}
+    </div>
+  </div>
+</div>
--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,40 +1,53 @@
 <script>
-	import Card from '$lib/components/Card.svelte';
-	export let data;
+  export let data
+  import CardComment from '../../../lib/components/CardComment.svelte'
 </script>
 
 <div class="card w-96 bg-primary text-primary-content">
-	<div class="card-body">
-		<h2 class="card-title">{data.comments[0].post.title}</h2>
-		<p>{data.comments[0].post.content}</p>
-		<p>{data.comments[0].post.author}</p>
-	</div>
+  <div class="card-body">
+    <h2 class="card-title">{data.comments[0].post.title}</h2>
+    <p>{data.comments[0].post.content}</p>
+    <p>Author: {data.comments[0].post.author.name}</p>
+  </div>
 </div>
 
 {#each data.comments as comment}
-	<Card author={comment.user.name} likes={comment.likes.length} title={comment.message} />
-	<form method="POST" action="?/comentarComentario">
-		<div class="card-actions">
-			<button class="btn btn-primary" formaction="?/like">Like</button>
-			<button class="btn btn-primary" formaction="?/dislike">Dislike</button> <br />
-			<input type="hidden" name="parentId" value={comment.id} />
-			<input type="hidden" name="userId" value={comment.userId} />
-			<input type="text" name="message" placeholder="leave a comment" />
-			<button class="btn" type="submit">Send</button>
-		</div>
-	</form>
-	{#if comment.children}
-		{#each comment.children as kid}
-			<div class="card w-96 bg-neutral text-neutral-content">
-				<div class="card-body items-center text-center">
-					<p>{kid.message}</p>
-					<p>I am a kid</p>
-					<div class="card-actions justify-end">
-						<button class="btn btn-primary">Accept</button>
-						<button class="btn btn-ghost">Deny</button>
-					</div>
-				</div>
-			</div>
-		{/each}
-	{/if}
+  <div class="static mt-10 m-2">
+    <CardComment
+      message={comment.message}
+      username={comment.user.name}
+      likes={comment.likes.length}
+      commentId={comment.id}
+      userId={comment.userId}
+    />
+
+    {#if comment.children}
+      {#each comment.children as son}
+        <div class="relative mb-4 left-10 top-4">
+          <CardComment
+            class=""
+            son={true}
+            message={son.message}
+            username={son.user.name}
+            likes={son.likes.length}
+            commentId={son.id}
+            userId={son.userId}
+          />
+        </div>
+        <!--
+      {#if kid.children}
+        {#each kid.children as nieto}
+          <CardComment
+            message={nieto.message}
+            username={nieto.user.name}
+            likes={nieto.likes.length}
+            commentId={nieto.id}
+            userId={nieto.userId}
+          />
+        {/each}
+      {/if}
+  -->
+      {/each}
+    {/if}
+  </div>
 {/each}
